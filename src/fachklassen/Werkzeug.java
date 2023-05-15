d5100cadbe553856709ae0f8228e40d9234703c8
info-git1-tk <133233845+info-git1-tk@users.noreply.github.com>
2023-05-15 19:48:53 +0200

    Add Class

---
+++ src/fachklassen/Werkzeug.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fachklassen;
+
+/**
+ *
+ * @author BenjaminWagner
+ */
+public class Werkzeug {
+    
+}
